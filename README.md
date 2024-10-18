5fc424833f1a2224ce7d9c2718536157283c941a
Dan Pop <hc7822@wayne.edu>
2024-10-18 12:01:39 -0400

    v2 with working captcha solver

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
-### Installation
-1) Before running the bot, run the following command:
-```
-npm install puppeteer axios dotenv
-```
-2) Create a .env file and fill it with the web url, key and the captcha extension api key
+# Installation
 
-3) Inside addToCart.js, on line 55, ensure that the absolute path to chrome.exe is correct on your system.
+### Before you can run the bot, follow these steps:
 
-### Running the bot
+1. Inside the AddToCartBot directory, run the following command in your terminal:
+
+```
+npm install puppeteer dotenv
+```
+
+2. Create a .env file and fill it with both the web URL and the CAPTCHA solver extension api key
+
+# Running the bot
+
+### Once the installation is complete, follow these steps:
+
+1. Close all open chrome pages. Open a terminal and run the command:
+
+```
+\path\to\chrome start chrome --remote-debugging-port=9222
+```
+
+- This command opens Chrome in a special mode that allows the bot to control it.
+- Make sure the CAPTCHA solver extension is enabled in this Chrome window, as the bot will connect to it.
+
+2. Returning back to the AddToCartBot directory, run the command:
+
 ```
 node addToCart.js
 ```
+
+- This command starts the bot, which will connect to the Chrome window you opened and begin interacting with the website.
--- addToCart.js
+++ addToCart.js
@@ -1,58 +1,13 @@
 const puppeteer = require('puppeteer')
-const axios = require('axios')
 require('dotenv').config();
 
-const API_KEY = process.env.API_KEY
-const WEBSITE_KEY = process.env.WEBSITE_KEY
 const WEBSITE_URL = process.env.WEBSITE_URL
-
-async function capSolver() {
-    const payload = {
-        clientKey: API_KEY,
-        task: {
-            type: 'ReCaptchaV2TaskProxyLess',
-            websiteKey: WEBSITE_KEY,
-            websiteURL: WEBSITE_URL,
-        }
-    };
-
-    try {
-        const res = await axios.post("https://api.capsolver.com/createTask", payload);
-        const task_id = res.data.taskId;
-        if (!task_id) {
-            console.log("Failed to create task:", res.data);
-            return null;
-        }
-        console.log("Got taskID:", task_id);
-
-        while (true) {
-            await new Promise(resolve => setTimeout(resolve, 1000)); // Delay for 1 second
-
-            const getResultPayload = { clientKey: API_KEY, taskId: task_id };
-            const resp = await axios.post("https://api.capsolver.com/getTaskResult", getResultPayload);
-            const status = resp.data.status;
-
-            if (status === "ready") {
-                return resp.data.solution.gRecaptchaResponse;
-            }
-            if (status === "failed" || resp.data.errorId) {
-                console.log("Solve failed! response:", resp.data);
-                return null;
-            }
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        return null;
-    }
-}
 
 async function main() {
     try {
-        const browser = await puppeteer.launch({
-            headless: false,                    // allows user to see puppeteer working in browser
-            defaultViewport: false,             // adjusts browser window size
-            args: ['--start-maximized'],        // opens browser in full screen
-            executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',   // specify the path to the Chrome executable
+        const browser = await puppeteer.connect({
+            browserURL: 'http://127.0.0.1:9222',
+            defaultViewport: false,
         });
         const page = await browser.newPage();
         await page.goto(WEBSITE_URL);
@@ -63,17 +18,11 @@ async function main() {
         await page.locator('[aria-label="Row #1 - hunt location code"]').fill("020")
         await page.locator('[aria-label="Row #1 - date period code"]').fill("O1")
         await page.locator('[aria-label="Row #1 - weapon code"]').fill("R")
-
-        /*capSolver().then(token => {
-            console.log(token);
-        })*/
         
         // throw the bot in a loop until the checkout button is enabled
         var isCheckoutBtnDisabled = true;
         while (isCheckoutBtnDisabled) {
-            console.log("Waiting for the checkout button to be disabled...");
             await page.waitForSelector('#submit.btn-primary.action-default.btn.btn-default.first-underline[accesskey="C"]', { timeout: 0 });
-            console.log("I see the checkout button is disabled."); 
             isCheckoutBtnDisabled = await page.evaluate(async () => {
                 const button = document.querySelector('#submit.btn-primary.action-default.btn.btn-default.first-underline.disabled[accesskey="C"]');
                 if (button != null) { // if the button still exists, that means the checkout button is still disabled
@@ -90,12 +39,13 @@ async function main() {
         var isAddToCartBtnVisible = false;
         var count = 0; //remove
         do {
-            console.log("About to click on the enabled checkout button...");
-            await page.click('#submit.btn-primary.action-default.btn.btn-default.first-underline[accesskey="C"]'); // click the enabled checkout button
+            console.log("Count " + count + ": About to click on the enabled checkout button...");
+            //await page.waitForSelector('#submit.btn-primary.action-default.btn.btn-default.first-underline[accesskey="C"]', { timeout: 0 });
+            await page.locator('#submit.btn-primary.action-default.btn.btn-default.first-underline[accesskey="C"]').click(); // click the enabled checkout button
             await new Promise(resolve => setTimeout(resolve, 100));
             // simulation of a tag being "dropped in"
             count += 1;//remove
-            if (count === 200 && !isAddToCartBtnVisible) {//remove
+            if (count === 1000 && !isAddToCartBtnVisible) {//remove
                 await new Promise(resolve => setTimeout(resolve, 1000));//remove
                 await page.locator('[aria-label="Row #1 - hunt location code"]').fill("012")//remove
                 await page.locator('[aria-label="Row #1 - date period code"]').fill("O4")//remove
@@ -114,9 +64,10 @@ async function main() {
         } while (!isAddToCartBtnVisible);
 
         // after the Confirm Choices page is rendered, the bot immediately clicks on the Add to Cart button
+        await new Promise(resolve => setTimeout(resolve, 100)); // need
         console.log("About to click on the Add to Cart button...");
         await page.waitForSelector('div[data-auto-id="action-bar-right"] a.btn-primary.action-default[accesskey="A"]', { timeout: 0 });
-        await page.click('div[data-auto-id="action-bar-right"] a.btn-primary.action-default[accesskey="A"]');
+        await page.locator('div[data-auto-id="action-bar-right"] a.btn-primary.action-default[accesskey="A"]').click();
         console.log("Add to Cart button clicked successfully.");
     }
     catch (error) {
